643be7cd9499f8f5c63b0798f5e65d204473ee87
edwiin1688 <chiisen@zoho.com>
2020-06-25 03:44:20 +0800

    區分 css 檔與 js 檔

---
+++ one-page-website/js/index.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    effect: 'fade',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+        delay: 2000
+    }
+});
