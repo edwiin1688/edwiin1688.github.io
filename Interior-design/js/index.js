39d8f929a43ac45b8bf89ea6a945b444011a7b2f
edwiin1688 <chiisen@zoho.com>
2020-07-01 14:50:32 +0800

    Item 改成 Swiper

---
+++ Interior-design/js/index.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    calculateHeight:true,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: 3000
+});
+
---
+++ Interior-design/js/items.js
@@ -0,0 +1,103 @@
+var items = [
+    {
+        itemType: 'soft',
+        id: 13,
+        img: './img/ia_3600000013.png',
+        name: 'soft 13',
+        price: '100',
+    },
+    {
+        itemType: 'soft',
+        id: 27,
+        img: './img/ia_3600000027.png',
+        name: 'soft 27',
+        price: '200',
+    },
+    {
+        itemType: 'soft',
+        id: 40,
+        img: './img/ia_3600000040.png',
+        name: 'soft 40',
+        price: '300',
+    },
+    {
+        itemType: 'soft',
+        id: 79,
+        img: './img/ia_3600000079.png',
+        name: 'soft 79',
+        price: '400',
+    },
+    {
+        itemType: 'soft',
+        id: 92,
+        img: './img/ia_3600000092.png',
+        name: 'soft 92',
+        price: '500',
+    },
+    {
+        itemType: 'light',
+        id: 156,
+        img: 'img/ia_1000000156.png',
+        name: 'light 156',
+        price: '100',
+    },
+    {
+        itemType: 'light',
+        id: 157,
+        img: 'img/ia_1000000157.png',
+        name: 'light 157',
+        price: '200',
+    },
+    {
+        itemType: 'light',
+        id: 158,
+        img: 'img/ia_1000000158.png',
+        name: 'light 158',
+        price: '300',
+    },
+    {
+        itemType: 'light',
+        id: 169,
+        img: 'img/ia_1000000169.png',
+        name: 'light 169',
+        price: '400',
+    }
+];
+
+var sofaMain = document.querySelector('.swiper-wrapper');
+
+items.forEach(e => {
+    sofaMain.innerHTML += `
+            <div class="swiper-slide">
+                <img id="img-${e.id}" src="${e.img}" itemType="${e.itemType}">
+            </div>
+            `;
+});
+
+items.forEach(e => {
+    let cardImg = document.querySelector(`#img-${e.id}`);
+    console.log(`${cardImg.id} add OnClick`);
+    cardImg.addEventListener("click", addSofaClick, false);
+});
+
+function addSofaClick(e) {
+    console.log(`${e.target.id} OnClick`);
+
+    const found = items.find(x => e.target.id == `img-${x.id}`);
+    if (found != undefined) {
+        
+        if(e.target.hasAttribute('itemType'))
+        {
+            let itemType = e.target.getAttribute('itemType');
+            let item;
+            if(itemType == 'soft')
+            {
+                item = document.querySelector('#sofa');
+            }
+            else{
+                item = document.querySelector('#light');
+            }
+            item.setAttribute('src', found.img);
+        }
+    }
+}
