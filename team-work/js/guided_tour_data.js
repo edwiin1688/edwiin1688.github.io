d90de1fb9183c87827207fc7e7b09e23d74112b7
edwiin1688 <chiisen@zoho.com>
2020-07-06 10:30:10 +0800

    fix: json error

--- team-work/js/guided_tour_data.js
+++ team-work/js/guided_tour_data.js
@@ -1,17 +1,17 @@
 var guided_tour_data = [
     {
         text: "美美的女生1",
-        src: "./img/girl-01.jpg",
+        src: "./img/guided_tour_1.png",
         subText: "女生1"
     },
     {
         text: "美美的女生2",
-        src: "./img/girl-02.jpg",
+        src: "./img/guided_tour_1.png",
         subText: "女生2"
     },
     {
         text: "美美的女生3",
-        src: "./img/girl-03.jpg",
+        src: "./img/guided_tour_1.png",
         subText: "女生3"
     }
 ];
