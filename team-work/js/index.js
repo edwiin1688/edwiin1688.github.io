a5f86f5cbeed7d375a6681e5c57db8441af9810f
edwiin1688 <chiisen@zoho.com>
2020-07-08 07:48:05 +0800

    安裝 parallax.js 離線版

--- team-work/js/index.js
+++ team-work/js/index.js
@@ -1,12 +1,3 @@
-
-
-// 快捷 ICON
-// var join = document.querySelector('#quick-icon');
-// join.addEventListener('click', function() {
-//     console.log('快捷 ICON');
-//     $('html, body').animate({scrollTop: $('#footer').offset().top}, 1000)
-// });
-
 // SCROLL DOWN
 var scroll_down = document.querySelector('#scroll_down');
 scroll_down.addEventListener('click', function() {
