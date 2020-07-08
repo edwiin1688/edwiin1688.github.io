61377fda9aa4d799f95bb4cf233fe6ee49f8e5bf
wei-kai-life <life201975itme@gmail.com>
2020-07-08 08:14:02 +0800

    Revert "fix:瀑布流"
    This reverts commit d5ef1fb3f0482695fcbf1c9b6fe5f68868b0b6bc.

--- team-work/js/masonry.js
+++ team-work/js/masonry.js
@@ -1,25 +1,11 @@
-$.getScript('./js/masonry-data.js', function () {
-
-    var masonry = document.querySelector('#masonry');
-    masonry.innerHTML += `
-        <h1 class="d-flex justify-content-center">瀑布流</h1>
-        <div class="masonry">
-            <!-- 動態放入瀑布流照片 -->
-        </div>
-        `;
-
-    // 動態加入瀑布流
-    var m = document.querySelector('.masonry');
-    masonry_data.forEach(x => {
-        m.innerHTML += `
-        <div class="item">
-            ${x.text}
-            <img
-                src="${x.src}">
-        </div>
-        `;
-    });
-
+// 動態加入瀑布流
+var m = document.querySelector('.masonry');
+masonry_data.forEach(x => {
+    m.innerHTML += `
+    <div class="item">
+        ${x.text}
+        <img
+            src="${x.src}">
+    </div>
+    `;
 });
-
-
