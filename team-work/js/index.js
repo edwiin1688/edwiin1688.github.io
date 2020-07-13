527d8ee1121558421673523cbd448aee8003471a
edwiin1688 <chiisen@zoho.com>
2020-07-13 16:25:48 +0800

    add: 650px hide banner

--- team-work/js/index.js
+++ team-work/js/index.js
@@ -5,3 +5,25 @@ scroll_down.addEventListener('click', function() {
     $('html, body').animate({scrollTop: $('#end').offset().top}, 1000)
 });
 
+var mm = window.matchMedia("(max-width: 650px)");
+mm.addListener(resizeWidth);
+resizeWidth(mm);
+
+function resizeWidth(pMatchMedia){
+  if (pMatchMedia.matches) {
+    //小於650px時執行的js
+    if ($('.banner').is(":visible")){
+        $('.banner').hide();
+        console.log('hide 大於650px時執行的js');
+    }
+    console.log('小於650px時執行的js');
+  }else {
+    //大於650px時執行的js
+    if ($('.banner').is(":hidden")){
+        $('.banner').show();
+        console.log('show 大於650px時執行的js');
+    }
+    
+    console.log('大於650px時執行的js');
+  }
+}
