672a7620bdfda1d1902fe9cd4e8509fa9a098dfb
edwiin1688 <chiisen@zoho.com>
2020-07-15 11:19:50 +0800

    1. update banner 2. 更新互動視窗

--- one-page-website/js/index.js
+++ one-page-website/js/index.js
@@ -16,20 +16,28 @@ var swiper = new Swiper('.bannr', {
 });
 
 var join = document.querySelector('.join');
-join.addEventListener('click', function() {
+join.addEventListener('click', function () {
     console.log('click');
 });
 
 // ?　回首頁
 var top = document.querySelector('#top');
-top.addEventListener('click', function() {
-    console.log('goto TOP');
+top.addEventListener('click', function (e) {
+    if (e.target.id == 'top') {
+        console.log('goto TOP');
 
-    jump('');
+        jump('');
+    }
 });
 
-function jump(h){
+function jump(h) {
     var url = location.href;               //Save down the URL without hash.
-    location.href = "#"+h;                 //Go to the target element.
-    history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
+    location.href = "#" + h;                 //Go to the target element.
+    history.replaceState(null, null, url);   //Don't like hashes. Changing it back.
 }
+
+// ! parallax.js
+// 新增 data-depth="0.2"
+// var parallaxInstance1 = new Parallax(document.getElementById("parallax1"));
+// var parallaxInstance2 = new Parallax(document.getElementById("parallax2"));
+// var parallaxInstance3 = new Parallax(document.getElementById("parallax3"));
