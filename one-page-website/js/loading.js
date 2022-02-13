7c76289380aa3c6b6c68d560ab595bd5d8035dbc
hsin0724 <hikari810367@gmail.com.tw>
2022-02-14 02:03:29 +0800

    修正美食分頁 & 首頁跑版

--- one-page-website/js/loading.js
+++
@@ -1,4 +0,0 @@
-// Loading 相依 jquery 2.2.4 (npm install jquery@2.2.4)
-$(window).load(function () {
-    $("#loader").fadeOut(1500);
-});
--- team-work/js/index.js
+++ team-work/js/index.js
@@ -1,15 +1,19 @@
 // ?　SCROLL DOWN
 var scroll_down = document.querySelector('#scroll_down');
 scroll_down.addEventListener('click', function() {
-    
-    
-    console.log('#banner: '+ $('#banner').offset().top);
-    console.log('#philosophyN: '+ $('#philosophy').offset().top);
-
-    var scroll_pos = $('#philosophy').offset().top;
-    console.log('SCROLL DOWN: '+ scroll_pos);
-
-    $('html, body').animate({ scrollTop:(scroll_pos) }, 1500);
+    // console.log('#banner: '+ $('#banner').offset().top);
+    // console.log('#philosophyN: '+ $('#philosophy').offset().top);
+    // var scroll_pos = $('#philosophy').offset().top;
+    // console.log('SCROLL DOWN: '+ scroll_pos);
+    // $('html, body').animate({ scrollTop:(scroll_pos) }, 500);
 });
 
+function testScroll(ev){
+    if(window.pageYOffset > 150){
+        document.getElementById("scroll_down").style.display = "none";
+    } else {
+        document.getElementById("scroll_down").style.display = "block";
+    }
+}
 
+window.onscroll = testScroll;
